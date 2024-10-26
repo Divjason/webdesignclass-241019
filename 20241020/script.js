2c14bee9a40a4d25c8792f3bc03a7d4777411d6a
Divjason <sejinpark@tcocs.com>
2024-10-26 18:50:30 +0900

    241026

--- 20241020/script.js
+++ 20241020/script.js
@@ -28,3 +28,29 @@ setInterval(() => {
   currentIndex = (currentIndex + 1) % slides.length;
   slidelist.style.marginTop = -(currentIndex * 400) + "px";
 }, 2000)
+
+/* tab */
+const tabMenus = document.querySelectorAll(".tabmenu > ul > li > a");
+tabMenus.forEach((item) => {
+  item.addEventListener("click", () => {
+    const parentLi = item.parentElement;
+    const liItems = parentLi.parentElement.children;
+    [...liItems].forEach((liItem) => {
+      liItem.classList.remove("active");
+    });
+    parentLi.classList.add("active");
+  });
+});
+
+/* modal */
+const firstList = document.querySelector(".notice li:first-child");
+const layer = document.querySelector("#layer");
+const btn = document.querySelector(".btn");
+
+firstList.addEventListener("click", () => {
+  layer.classList.add("active");
+});
+
+btn.addEventListener("click", () => {
+  layer.classList.remove("active");
+})
